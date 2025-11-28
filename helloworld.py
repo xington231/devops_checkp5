6ac83511bf33f62feafa5a412997a0ab1722383b
Alina <degtyareva_18@mail.com>
2025-11-28 19:12:44 +0300

    feature/check_name add check_name

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,8 @@
-print("Введите ваше имя: ")
-name = input()
-print("Привет, " + name)
+while True:
+    name = input("Введите ваше имя: ").strip()
+    if name:
+        name = name.title()
+        print(f"Привет, {name}!")
+        break
+    else:
+        print("Имя не может быть пустым. Пожалуйста, введите имя.")
