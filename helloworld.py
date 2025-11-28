ebfa956831907215596b5c956b6769711a19b7b6
Alina <degtyareva_18@mail.com>
2025-11-28 19:24:57 +0300

    hotfix/fix_check_name fix

--- helloworld.py
+++ helloworld.py
@@ -1,12 +1,20 @@
+import re
+def is_valid_name(name):
+    pattern = r"^[A-Za-zА-Яа-яЁёs-]+$"
+    return bool(re.match(pattern, name))
 while True:
     try:
         name = input("Введите ваше имя: ").strip()
-        if name:
-            name = name.title()
-            print(f"Привет, {name}!")
-            break
-        else:
+        if not name:
             print("Имя не может быть пустым. Пожалуйста, введите имя.")
+            continue
+        if not is_valid_name(name):
+            print("Имя должно содержать только буквы, пробелы или дефисы. Попробуйте снова.")
+            continue
+
+        name = ' '.join(word.capitalize() for word in name.split())
+        print(f"Привет, {name}!")
+        break
     except KeyboardInterrupt:
         print("\nВыход из программы.")
         break
