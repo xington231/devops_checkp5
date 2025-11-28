49b0994a5d642c337c9836de554649a2f4d46281
Alina <degtyareva_18@mail.com>
2025-11-28 19:19:54 +0300

    hotfix/greating  fix

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,12 @@
 while True:
-    name = input("Введите ваше имя: ").strip()
-    if name:
-        name = name.title()
-        print(f"Привет, {name}!")
+    try:
+        name = input("Введите ваше имя: ").strip()
+        if name:
+            name = name.title()
+            print(f"Привет, {name}!")
+            break
+        else:
+            print("Имя не может быть пустым. Пожалуйста, введите имя.")
+    except KeyboardInterrupt:
+        print("\nВыход из программы.")
         break
-    else:
-        print("Имя не может быть пустым. Пожалуйста, введите имя.")
